864db9cb96c63d4bb94309ef4dab0e77d474dd72
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2024-12-04 18:17:01 +0200

    ...

--- TheLighthouseWavesPlayerApp/Platforms/Android/MainActivity.cs
+++ TheLighthouseWavesPlayerApp/Platforms/Android/MainActivity.cs
@@ -4,7 +4,7 @@ using Android.OS;
 
 namespace TheLighthouseWavesPlayerApp;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop,
+[Activity(Theme = "@style/Maui.SplashTheme", ResizeableActivity = true, MainLauncher = true, LaunchMode = LaunchMode.SingleTask,
     ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode |
                            ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
