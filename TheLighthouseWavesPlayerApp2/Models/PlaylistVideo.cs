72afe8697bbeba34c8934650621dc14fe6bfdcb0
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2024-12-16 12:54:43 +0200

    app2 add models

---
+++ TheLighthouseWavesPlayerApp2/Models/PlaylistVideo.cs
@@ -0,0 +1,12 @@
+﻿using SQLite;
+
+namespace TheLighthouseWavesPlayerApp2.Models;
+
+public class PlaylistVideo
+{
+    [PrimaryKey, AutoIncrement] public int Id { get; set; }
+
+    public int PlaylistId { get; set; }
+
+    public int VideoId { get; set; }
+}
