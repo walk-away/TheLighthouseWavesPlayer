96b04ec322c8ee12295c706f662aca95b1c1e68e
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2025-04-20 16:49:44 +0300

    added UpdateOrientationState...

--- TheLighthouseWavesPlayerVideoApp/Views/VideoPlayerPage.xaml.cs
+++ TheLighthouseWavesPlayerVideoApp/Views/VideoPlayerPage.xaml.cs
@@ -12,7 +12,23 @@ public partial class VideoPlayerPage : ContentPage
         InitializeComponent();
         BindingContext = viewModel;
         _viewModel = viewModel;
+
+        SizeChanged += OnPageSizeChanged;
     }
+
+    private void OnPageSizeChanged(object sender, EventArgs e)
+    {
+        UpdateOrientationState();
+    }
+
+    private void UpdateOrientationState()
+    {
+        string orientationState = Width > Height ? "Landscape" : "Portrait";
+        VisualStateManager.GoToState(this, orientationState);
+        
+        Shell.SetNavBarIsVisible(this, orientationState == "Portrait");
+    }
+
 
     void MediaElement_StateChanged(object sender, MediaStateChangedEventArgs e)
     {
@@ -27,12 +43,13 @@ public partial class VideoPlayerPage : ContentPage
     {
         base.OnNavigatedFrom(args);
         _viewModel?.OnNavigatedFrom();
-
+        
         if (mediaElement.CurrentState == MediaElementState.Playing ||
             mediaElement.CurrentState == MediaElementState.Paused)
         {
             mediaElement.Stop();
         }
+        SizeChanged -= OnPageSizeChanged;
     }
 
     protected override async void OnAppearing()
@@ -42,5 +59,8 @@ public partial class VideoPlayerPage : ContentPage
         {
             await _viewModel.CheckFavoriteStatusAsync();
         }
+        UpdateOrientationState();
+        // Re-subscribe if needed (though SizeChanged should trigger on appear if size changes)
+        // SizeChanged += OnPageSizeChanged; // Usually not needed here if subscribed in constructor
     }
 }
