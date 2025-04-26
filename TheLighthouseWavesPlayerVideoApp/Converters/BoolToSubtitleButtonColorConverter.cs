8d2f17696c36d958623dca356e33cfec84896fcc
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2025-04-26 09:52:13 +0300

    ImageButton in VideoPlayerPage...

---
+++ TheLighthouseWavesPlayerVideoApp/Converters/BoolToSubtitleButtonColorConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace TheLighthouseWavesPlayerVideoApp.Converters;
+
+public class BoolToSubtitleButtonColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool isEnabled)
+        {
+            return isEnabled ? Color.FromArgb("#008080") : Color.FromArgb("#808080");
+        }
+        
+        return Color.FromArgb("#808080");
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
